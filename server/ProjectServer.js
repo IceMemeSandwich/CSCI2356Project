f5622d2cdd37d2548fbefcdbb42bb26a7943e35b
Devin Robar <robarflynn@gmail.com>
2022-04-04 22:53:21 -0300

    Started changing server to use mySQL

--- server/ProjectServer.js
+++ server/ProjectServer.js
@@ -9,40 +9,57 @@
  const app = express(); // define top level function
  const port = 3111;
  
-// for now we only need 3 blogs
-let posts = {
-    '1':{
-       "title":"",
-       "post":"",
-       "posted": false
-    },
-    '2':{
-       "title":"",
-       "post":"",
-       "posted": false
-    },
-    '3':{
-       "title":"",
-       "post":"",
-       "posted": false
-    }
- };
+// Connecting to the database (I'l put mine in for now)
+// had to run this command on mysql server to get it working
+let database = mysql.createConnection({
+host: "127.0.0.1",
+user: "d_robar",
+password: "",  //! Leaving password blank for git
+database: "d_robar",
+connectTimeout: 10000,
+});
+database.connect();
  
+// These are the commands that are used to make the tables that will be used
+// CREATE TABLE Posts(
+//    id INT PRIMARY KEY,
+//    title VARCHAR(255),
+//    post TEXT,
+//    isPosted BOOLEAN NOT NULL
+// );
+
+// CREATE TABLE WordBank(
+//    id INT PRIMARY KEY AUTO_INCREMENT,
+//    word VARCHAR(255) NOT NULL UNIQUE
+// );
+
+
 app.use(express.json()); // implement JSON recognition
 app.use(express.urlencoded({ extended: true })); // implement incoming name:value pairs to be any type
 
 let allowCrossDomain = function (req, res, next) {
-res.header("Access-Control-Allow-Origin", "*"); // allow any origin
-res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE"); // allow any method
-res.header("Access-Control-Allow-Headers", "Content-Type"); // accept only headers with this type
-next(); // middleware callback function required for processing
+  res.header("Access-Control-Allow-Origin", "*"); // allow any origin
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE"); // allow any method
+  res.header("Access-Control-Allow-Headers", "Content-Type"); // accept only headers with this type
+  next(); // middleware callback function required for processing
 };
 app.use(allowCrossDomain); // implement allowable domain characteristics
 
 // setting input boxes at page load
-app.get("/receive", function (req, res) {
-    console.log(req.url);
-    return res.status(200).send(posts);
+app.get("/receive/:id", function (req, res) {
+    console.log(req.params.id);
+    database.query(
+      "SELECT * FROM Posts WHERE id=?;",
+      [request.params.id],
+
+      function (err, res) {
+        if (err)
+          console.log(
+            "An error has been thrown while searching for your record."
+          );
+        return res.status(200).send(req.params.id);
+      }
+    );
   });
 
 // template of receiving:
@@ -67,6 +84,13 @@ app.post("/send", function (req, res) {
          break;
     }
     console.log(posts);
+});
+
+
+process.on("SIGTERM", function () {
+  console.log("Shutting server down.");
+  database.close();
+  app.close();
 });
 
 // makes this program run on the port that was set at the top of the script
